941ae78b28d57c91c594493471ed870cdcfdaab0
izuku0550 <hyunbin0550@kakao.com>
2023-05-26 10:28:11 +0900

    UPDATE: rename data/data.rs->msg.rs

---
+++ src/data/msg.rs
@@ -0,0 +1,40 @@
+// #[cfg(feature="CLEAN")]
+pub const MSGS: [&str; 26] = [
+    "YOU KILLED MY TROJAN!\r\nNow you are going to die.",
+    "REST IN PISS, FOREVER MISS.",
+    "I WARNED YOU...",
+    "HAHA N00B L2P G3T R3KT",
+    "You failed at your 1337 h4x0r skillz.",
+    "YOU TRIED SO HARD AND GOT SO FAR, BUT IN THE END, YOUR PC WAS STILL FUCKED!",
+    "HACKER!\r\nENJOY BAN!",
+    "GET BETTER HAX NEXT TIME xD",
+    "HAVE FUN TRYING TO RESTORE YOUR DATA :D",
+    "|\\/|3|\\/|2",
+    "BSOD INCOMING",
+    "VIRUS PRANK (GONE WRONG)",
+    "ENJOY THE NYAN CAT",
+    "Get dank antivirus m9!",
+    "You are an idiot!\r\nHA HA HA HA HA HA HA",
+    "#MakeMalwareGreatAgain",
+    "SOMEBODY ONCE TOLD ME THE MEMZ ARE GONNA ROLL ME",
+    "Why did you even tried to kill MEMZ?\r\nYour PC is fucked anyway.",
+    "SecureBoot sucks.",
+    "gr8 m8 i r8 8/8",
+    "Have you tried turning it off and on again?",
+    "<Insert Joel quote here>",
+    "Greetings to all GAiA members!",
+    "Well, hello there. I don't believe we've been properly introduced. I'm Bonzi!",
+    "'This is everything I want in my computer'\r\n - danooct1 2016",
+    "'Uh, Club Penguin. Time to get banned!'\r\n - danooct1 2016",
+];
+
+pub const MEMZ_MSGBOXA_1: &str = "The software you just executed is considered malware.\r\n\
+This malware will harm your computer and makes it unusable.\r\n\
+If you are seeing this message without knowing what you just executed, simply press No and nothing will happen.\r\n\
+If you know what this malware does and are using a safe environment to test, \
+press Yes to start it.\r\n\r\n\
+DO YOU WANT TO EXECUTE THIS MALWARE, RESULTING IN AN UNUSABLE MACHINE?";
+
+pub const MEMZ_MSGBOXA_2: &str = "THIS IS THE LAST WARNING!\r\n\r\n\
+THE CREATOR IS NOT RESPONSIBLE FOR ANY DAMAGE MADE USING THIS MALWARE!\r\n\
+STILL EXECUTE IT?";
